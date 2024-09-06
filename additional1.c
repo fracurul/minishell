45dd53fb2ecd1a49962471946d8695f173fa485e
Fracurul <soloesunemail.1@gmail.com>
2024-09-06 20:02:20 +0200

    add

--- additional1.c
+++ additional1.c
@@ -4,15 +4,17 @@ int check_line(char *line)
 {
 	int i;
 	int j;
+	int k;
 
 	i = 0;
 	j = 0;
+	len = ft_strlen(line);
 
-	while(input && input != '\\' && input != ';')
+	while(line && line != '\\' && line != ';' && line[0] != '|' && line[len - 1] != '|')
 	{
-		if(input == '\'')
+		if(line == '\'')
 			i++;
-		if(input == '"')
+		if(line == '"')
 			j++;
 	}
 	if(i % 2 == 0 && j % 2 == 0)
